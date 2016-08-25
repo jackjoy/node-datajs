8540e1fd0f9a4d5c2c08bd28bdfef83a1d78ce7b
Eric Tang <etang@metavine.com>
2016-08-25 10:12:54 +1000

    add the missing files from last commit

---
+++ build/README.md
@@ -0,0 +1,2 @@
+
+node-datajs - an odata js javascript library fork with some tweaks
---
+++ build/exports.js
@@ -0,0 +1,4 @@
+// Export OData
+if (typeof module !== 'undefined' && module.exports) {  
+    module.exports = window.OData;
+}
